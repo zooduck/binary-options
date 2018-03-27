7d5465e15f1d6d77125ea48c4dbcd7195f1a9e2d
ZOODUCK <zooduck@beniceoreatsomerice.com>
2018-03-27 17:41:41 +0100

    add support for accessing simple variables (and not just objects) with data-template-bind, using an optional data-template-item attribute.

--- src/scripts/services/view_binder.service.js
+++ src/scripts/services/view_binder.service.js
@@ -9,7 +9,7 @@ export const viewBinderService = (function () {
 					el.parentNode.removeChild(child);
 				}
 			}
-		}		
+		}
 	};
 
 	const $setVal = (el, viewVal, dataBind) => {
@@ -25,12 +25,12 @@ export const viewBinderService = (function () {
 		// UPDATE ALL data-bind type ELEMENTS IN THE DOM (much like $scope.$apply() in AngularJS)
 		// =========================================================================================
 		let boundEls = document.querySelectorAll("[data-bind]");
-		for (const el of boundEls) {			
+		for (const el of boundEls) {
 			const evalStr = `zoobinary.${el.dataset.bind}`;
 			const viewVal = eval(evalStr);
 			if (!$setVal(el, viewVal, el.dataset.bind)) {
 				console.error(`[data-bind] value for ${el.dataset.bind} not found`);
-			}						
+			}
 		}
 		const boundCheckboxEls = document.querySelectorAll("[data-checked]");
 		for (const el of boundCheckboxEls) {
@@ -45,21 +45,36 @@ export const viewBinderService = (function () {
 		for (const dataTemplate of dataTemplates) {
 
 			const evalStr = `zoobinary.${dataTemplate.dataset.template}`;
+			const itemName = dataTemplate.dataset.templateItem; // This is an optional item reference assigned to the with data-template-item attribute
 			const templateArray = eval(evalStr);
-			
+
 			for (const item of Array.from(templateArray)) {
 				const templateEl = dataTemplate.cloneNode(true);
 				templateEl.removeAttribute("data-template");
 				const boundEls = templateEl.querySelectorAll("[data-template-bind]");
-								
+
 				for (const boundEl of boundEls) {
-					const evalStr = `item.${boundEl.dataset.templateBind}`;
-					const viewVal = eval(evalStr);					
+					let boundElVal = boundEl.dataset.templateBind;
+					if (itemName) {
+						if (!boundEl.dataset.templateBind.match(itemName)) {
+							// hard fail
+							return console.error(`${itemName} not found in ${boundElVal}. Check that the item reference for data-template-item (in your template) matches the item reference for data-template-bind.`);
+						}
+						boundElVal = boundEl.dataset.templateBind.replace(itemName, "");
+					}
+					const evalStr = itemName? `item${boundElVal}` : `item.${boundElVal}`;
+					let viewVal = "";
+					try {
+						viewVal = eval(evalStr);
+					} catch (err) {
+						// ...
+					}
 					if (!$setVal(boundEl, viewVal, boundEl.dataset.templateBind)) {
+						// soft fail
 						console.error(`[data-template-bind] value for ${boundEl.dataset.templateBind} not found`);
 					}
 				}
-				dataTemplate.parentNode.appendChild(templateEl);				
+				dataTemplate.parentNode.appendChild(templateEl);
 			}
 		}
 	};
